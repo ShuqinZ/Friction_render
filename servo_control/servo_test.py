289cc4b41f1cb6cb4a76278553cb696317d17070
Shuqin <shuqinzhu37@gmail.com>
2025-03-25 13:42:30 -0700

    update pin connect

--- servo_control/servo_test.py
+++ servo_control/servo_test.py
@@ -6,32 +6,31 @@ from time import sleep
 
 # minimum pulse width, and maximum pulse width
 
-servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
+servo = AngularServo(14, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 try:
 
-   while True:
+    while True:
+        # Set the servo angle to 90 degrees
 
-       # Set the servo angle to 90 degrees
+        servo.angle = 90
 
-       servo.angle = 90
+        sleep(1)  # Delay for 1 second
 
-       sleep(1)  # Delay for 1 second
+        # Set the servo angle to 0 degrees
 
-       # Set the servo angle to 0 degrees
+        servo.angle = 0
 
-       servo.angle = 0
+        sleep(1)  # Delay for 1 second
 
-       sleep(1)  # Delay for 1 second
+        # Set the servo angle to -90 degrees
 
-       # Set the servo angle to -90 degrees
+        servo.angle = -90
 
-       servo.angle = -90
-
-       sleep(1)  # Delay for 1 second
+        sleep(1)  # Delay for 1 second
 
 finally:
 
-   # Set the servo angle to 0 degrees before exiting
+    # Set the servo angle to 0 degrees before exiting
 
-   servo.angle = 0
+    servo.angle = 0
