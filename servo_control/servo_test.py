437ca923a0aef0b91787b89784845c4494ff737d
Shuqin <shuqinzhu37@gmail.com>
2025-03-25 14:07:07 -0700

    update angle

--- servo_control/servo_test.py
+++ servo_control/servo_test.py
@@ -26,10 +26,10 @@ signal.signal(signal.SIGTERM, cleanup)
 # Example: sweep servo back and forth
 try:
     while True:
-        for angle in range(-90, 91, 10):
+        for angle in range(0, 180):
             servo.angle = angle
-            sleep(0.05)
-        for angle in range(90, -91, -10):
+            # sleep(0.05)
+        for angle in range(180, 0, -1):
             servo.angle = angle
             sleep(0.05)
 
