be15ecd1e186ee2473f900c037daa281f6192ec1
Shuqin <shuqinzhu37@gmail.com>
2025-04-01 15:31:33 -0700

    fix pwm pin usage

--- utils/pi5RC.py
+++ utils/pi5RC.py
@@ -17,14 +17,14 @@ class pi5RC:
             # let's set pin ctrl
             os.system("/usr/bin/pinctrl set {} {}".format(self.pin, afunc[self.pinIdx]))
             # let export pin
-            if not os.path.exists("/sys/class/pwm/pwmchip2/pwm{}".format(self.pwmx[self.pinIdx])):
-                os.system("echo {} > /sys/class/pwm/pwmchip2/export".format(self.pwmx[self.pinIdx]))
+            if not os.path.exists("/sys/class/pwm/pwmchip0/pwm{}".format(self.pwmx[self.pinIdx])):
+                os.system("echo {} > /sys/class/pwm/pwmchip0/export".format(self.pwmx[self.pinIdx]))
             # CLOCK AT 1gHZ  let put period to 20ms
             time.sleep(0.2)
-            os.system("echo 20000000 > /sys/class/pwm/pwmchip2/pwm{}/period".format(self.pwmx[self.pinIdx]))
+            os.system("echo 20000000 > /sys/class/pwm/pwmchip0/pwm{}/period".format(self.pwmx[self.pinIdx]))
             time.sleep(0.1)
             self.enable(False)
-            self.file_duty = open("/sys/class/pwm/pwmchip2/pwm{}/duty_cycle".format(self.pwmx[self.pinIdx]), "w")
+            self.file_duty = open("/sys/class/pwm/pwmchip0/pwm{}/duty_cycle".format(self.pwmx[self.pinIdx]), "w")
             if self.file_duty.closed:
                 raise IOError("Unable to create pwm{} file".format(self.pwmx[self.pinIdx]))
         else:
@@ -33,13 +33,13 @@ class pi5RC:
 
     def enable(self, flag):
         self.enableFlag = flag
-        os.system("echo {} > /sys/class/pwm/pwmchip2/pwm{}/enable".format(
+        os.system("echo {} > /sys/class/pwm/pwmchip0/pwm{}/enable".format(
             int(self.enableFlag), self.pwmx[self.pinIdx]))
 
     def __del__(self):
         if self.pin is not None:
             # ok take PWM out
-            os.system("echo {} > /sys/class/pwm/pwmchip2/unexport".format(self.pwmx[self.pinIdx]))
+            os.system("echo {} > /sys/class/pwm/pwmchip0/unexport".format(self.pwmx[self.pinIdx]))
             # disable PWM Pin
             os.system("/usr/bin/pinctrl set {} no".format(self.pin))
             if not self.file_duty.closed:
