4c3a02863ed4c432ccb667146eaa8d1be6d38eae
Shuqin <shuqinzhu37@gmail.com>
2025-04-01 15:28:15 -0700

    test hardware pwm

---
+++ servo_control/servo_hardware_pwm.py
@@ -0,0 +1,16 @@
+from utils.pi5RC import pi5RC
+from time import sleep
+
+servo = pi5RC(18)  # GPIO18 is PWM-capable on Pi 5
+
+# Sweep servo slowly
+try:
+    while True:
+        for pulse in range(500, 2500, 10):  # microseconds
+            servo.set(pulse)
+            sleep(0.01)
+        for pulse in range(2500, 500, -10):
+            servo.set(pulse)
+            sleep(0.01)
+except KeyboardInterrupt:
+    del servo
