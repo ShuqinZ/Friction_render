b4d20a55c6bd436668748c468abe6a84af1d251d
Shuqin <shuqinzhu37@gmail.com>
2025-04-02 10:53:39 -0700

    update

--- friction_render.py
+++ friction_render.py
@@ -4,7 +4,8 @@ import busio
 import numpy as np
 from adafruit_ads1x15.ads1115 import ADS1115
 from adafruit_ads1x15.analog_in import AnalogIn
-from pi5RC import pi5RC  # your fixed class
+
+from utils.pi5RC import pi5RC
 
 # === Hardware Setup ===
 i2c = busio.I2C(board.SCL, board.SDA)
