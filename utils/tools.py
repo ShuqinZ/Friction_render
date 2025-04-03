75381ffab32fdb9e138b0177aee683f12b0e4fa8
Shuqin <shuqinzhu37@gmail.com>
2025-04-03 09:24:00 -0700

    fix compression calculation

--- utils/tools.py
+++ utils/tools.py
@@ -5,5 +5,5 @@ def cross(a: np.ndarray, b: np.ndarray) -> np.ndarray:
     return np.cross(a, b)
 
 def read_potentialmeter(raw):
-    pos = (((raw / 32767.0) * 10.5) / 1.01 + 1  # same mapping as your main script
+    pos = ((raw / 32767.0) * 10.5) / 1.01 + 1  # same mapping as your main script
     return pos
