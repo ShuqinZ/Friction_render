76cdf4dc8d05c850e3a5d79e0a3bdfdd74a84313
Shuqin <shuqinzhu37@gmail.com>
2025-04-03 15:14:19 -0700

    tune pid

--- utils/pi5RC.py
+++ utils/pi5RC.py
@@ -45,8 +45,8 @@ class pi5RC:
         self.enableFlag = flag
         self._write(f"{self.pwm_path}/enable", "1" if flag else "0")
 
-    def set(self, angle: float, angle_range: float = 180.0, pulse_range: tuple = (500, 2400)):
-        pulse_width = int((angle / angle_range) * (pulse_range[1] - pulse_range[0]) + pulse_range[0])
+    def set(self, angle: float, angle_range: float = 180.0, pulse_range: tuple = (5, 24)):
+        pulse_width = int(round((angle / angle_range) * (pulse_range[1] - pulse_range[0]) + pulse_range[0])) * 100
         self.set_pwm(pulse_width)
 
     def set_pwm(self, onTime_us: int):
