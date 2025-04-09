2d6c96702178554dcae4f399e367c8822f01b050
Shuqin <shuqinzhu37@gmail.com>
2025-04-09 16:48:09 -0700

    update

--- friction_render.py
+++ friction_render.py
@@ -148,7 +148,9 @@ try:
             previous_error = error
 
             positionChange = high_pass_alpha * (positionChange + targetPosition - lastTargetPosition)
-            pid_scale_factor = 1 + np.tanh(abs(positionChange)) * 4 if abs(positionChange) > 0.2 else 1
+
+            baseScale = 2 if sliding else 1
+            pid_scale_factor = baseScale + np.tanh(abs(positionChange)) * 4 if abs(positionChange) > 0.2 else 1
 
             error_percent = 100 * (detectedForce - frictionForce) / frictionForce if frictionForce > 0 else 0
 
