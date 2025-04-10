61eb4f421179c3fd87ba31f6bda5eb6c166ee5c7
Shuqin <shuqinzhu37@gmail.com>
2025-04-09 17:31:38 -0700

    update

--- friction_render.py
+++ friction_render.py
@@ -126,7 +126,7 @@ try:
 
             # === PID ===
             if calibrated and external_velocity > delta_v:
-                targetPosition -= external_velocity * 2 * dt
+                targetPosition -= external_velocity * 1.5 * dt
             velocity = (smoothedPosition - lastSmoothedPosition) / dt
             error = targetPosition - smoothedPosition
             integral += error * dt
