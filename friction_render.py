cedfa2470ac60e5649489795964b9499188de7b1
Shuqin <shuqinzhu37@gmail.com>
2025-04-10 09:40:48 -0700

    update

--- friction_render.py
+++ friction_render.py
@@ -129,7 +129,7 @@ try:
                 # if velocity - motorVelocity > delta_v:
                 targetPosition -= external_velocity * 1.2 * dt
                 if sliding:
-                    targetPosition -= external_velocity * 1.5 * dt
+                    targetPosition -= external_velocity * 1.6 * dt
             velocity = (smoothedPosition - lastSmoothedPosition) / dt
             error = targetPosition - smoothedPosition
             integral += error * dt
@@ -156,9 +156,9 @@ try:
             pid_scale_factor = 1
             if calibrated:
                 if sliding:
-                    pid_enhance = pid_enhance + min(np.tanh(abs(positionChange)), 0.1)
+                    pid_enhance = pid_enhance + np.tanh(abs(positionChange))
                 if external_velocity > delta_v:
-                    pid_enhance = pid_enhance + np.tanh(abs(external_velocity/20))
+                    pid_enhance = pid_enhance + np.tanh(abs(external_velocity/50))
 
             pid_scale_factor += pid_enhance
 
