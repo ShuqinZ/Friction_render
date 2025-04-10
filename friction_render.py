5ea4d59f0d272d6b98baf0026c4bcb4725d7440b
Shuqin <shuqinzhu37@gmail.com>
2025-04-09 17:11:22 -0700

    update

--- friction_render.py
+++ friction_render.py
@@ -155,7 +155,7 @@ try:
                 # if sliding:
                 #     pid_enhance = pid_enhance + 0.2 + np.tanh(abs(positionChange))
                 if external_velocity > delta_v:
-                    pid_enhance = pid_enhance + 1 + np.tanh(abs(external_velocity))
+                    pid_enhance = pid_enhance + 0.2 + np.tanh(abs(external_velocity))
 
             pid_scale_factor += pid_enhance
 
