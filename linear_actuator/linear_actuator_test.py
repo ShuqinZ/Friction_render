9d3d49ff82ad557655f63bba01979440afe7f5a6
Shuqin <shuqinzhu37@gmail.com>
2025-04-17 14:09:56 -0700

    update

--- linear_actuator/linear_actuator_test.py
+++ linear_actuator/linear_actuator_test.py
@@ -12,7 +12,7 @@ lgpio.gpio_write(h, DIR, 1)  # Set direction
 
 print("Sending step pulses to motor...")
 for i in range(200):  # One revolution (if 1.8° per step)
-    lgpio.gpio_write(h, STEP, 0)
+    lgpio.gpio_write(h, STEP, 1)
     time.sleep(0.01)
 
 lgpio.gpiochip_close(h)
